cf75975d61f1ad13ffd025a775caf080dd508eb4
benny <b1623546466@gmail.com>
2025-04-03 11:19:20 +0000

    fix: export function for fix twitter username

--- packages/task-manager/src/index.ts
+++ packages/task-manager/src/index.ts
@@ -3,5 +3,13 @@ import { SharedService } from './shared/shared.service.js';
 import { TwitterClientStarter } from './shared/starter.service.js';
 import { TasksService } from './tasks/tasks.service.js';
 import { TasksModule } from './tasks/tasks.module.js';
+import { autoFixTwitterUsername } from './tasks/schemas/task.schema.js';
 
-export { TaskManagerModule, SharedService, TwitterClientStarter, TasksService, TasksModule };
+export {
+    TaskManagerModule,
+    SharedService,
+    TwitterClientStarter,
+    TasksService,
+    TasksModule,
+    autoFixTwitterUsername
+};
